b4733e9a9067aef014c819c41274ec763d49b384
Alfredo Castaneda <70920990+casalf724@users.noreply.github.com>
2023-12-16 02:46:49 +0000

    Draft

--- src/app.py
+++ src/app.py
@@ -8,6 +8,7 @@ load_dotenv()
 
 # 1) Connect to the database here using the SQLAlchemy's create_engine function
 
+
 # 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
 
 # 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
